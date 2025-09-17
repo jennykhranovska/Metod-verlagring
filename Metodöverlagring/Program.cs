220fbce762ab8e6f2b21d44418384787b1e4ee3e
Jenny Khranovska <jenny.khranovska.syne25lin@edu.tucsweden.se>
2025-09-17 11:01:34 +0200

    Add project files.

---
+++ Metodöverlagring/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace Metodöverlagring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FormatName("Anna", "Svensson"));
+            
+
+            
+            Console.WriteLine(FormatName("Anna", "Maria", "Svensson")); 
+
+
+            
+            Console.WriteLine(FormatName("Anna", "Svensson", true)); 
+
+        }
+
+        static string FormatName(string förnamn, string efternamn)
+        {
+            return $"{förnamn} {efternamn}"; // Sammanfogar förnamn och efternamn
+        }
+
+        // Andra versionen av FormatName som tar tre parametrar: förnamn, mellannamn och efternamn
+        static string FormatName(string förnamn, string mellannamn, string efternamn)
+        {
+            return $"{förnamn} {mellannamn} {efternamn}"; // Sammanfogar förnamn, mellannamn och efternamn
+        }
+
+        static string FormatName(string förnamn, string efternamn, bool uppercase)
+        {
+            var full = $"{förnamn} {efternamn}"; // Sammanfoga förnamn och efternamn
+            return uppercase ? full.ToUpper() : full;
+
+
+
+
+
+
+        }
+
+    }
+}
